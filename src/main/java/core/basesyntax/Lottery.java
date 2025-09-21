a5113ba7291a94b3551629b57218c14ef36c052e
Viktoriia <fvee737@gmail.com>
2025-09-21 13:31:24 +0200

    generation of a random ball

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall() {
+        String randomColor = colorSupplier.getRandomColor().toString();
+        int randomNumber = random.nextInt(101);
+        return new Ball(randomColor, randomNumber);
+    }
+}
