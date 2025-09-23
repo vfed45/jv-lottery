977a950d2fe6fd01e6fcec4ace0f02629f20a33b
Viktoriia <fvee737@gmail.com>
2025-09-23 20:36:41 +0200

    done2

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 101;
+
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
-
-    private static final int MAX_BALL_NUMBER = 101;
 
     public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor().name(), random.nextInt(MAX_BALL_NUMBER));
